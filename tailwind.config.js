fdc1f07cbd917bbb8204c54928aa92334a5028c7
Farad Alam <farad.dev@gmail.com>
2025-01-29 01:04:01 +0600

    footer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "black", "luxury"],
+    themes: ["light", "dark", "black", "luxury","night"],
   },
 };
